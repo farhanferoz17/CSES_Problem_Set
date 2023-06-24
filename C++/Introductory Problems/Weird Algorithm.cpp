1d2be63d44122f89549a7a09ca62b842e78c188d
Farhan Feroz <38178160+farhanferoz17@users.noreply.github.com>
2023-06-25 03:06:36 +0600

    Update Weird Algorithm.cpp

--- C++/Introductory Problems/Weird Algorithm.cpp
+++ C++/Introductory Problems/Weird Algorithm.cpp
@@ -1,1 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+  long long int n;
+  cin >> n;
+  while(n!=1){
+    cout << n << " ";
+    if(n&1){
+      n = 3*n+1;
+    }
+    else{
+      n /= 2;
+    }
+  }
+  cout << "1\n";
+  return 0;
+}
