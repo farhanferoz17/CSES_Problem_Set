1c6e8fee7228e6a8fb03178ecce0dff554ce5c73
Farhan Feroz <38178160+farhanferoz17@users.noreply.github.com>
2023-06-25 03:13:34 +0600

    Update Distinct Numbers.cpp

--- C++/Sorting and Searching/Distinct Numbers.cpp
+++ C++/Sorting and Searching/Distinct Numbers.cpp
@@ -1,1 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+  int n;
+  cin >> n;
+  vector<int> nums(n);
+  for(int i=0;i<n;i++){
+    cin >> nums[i];
+  }
+  sort(nums.begin(), nums.end());
+  int ct = 1;
+  for(int i=1;i<n;i++){
+    if(nums[i]!=nums[i-1]){
+      ct++;
+    }
+  }
+  cout << ct << endl;
+  return 0;
+}
